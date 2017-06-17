274d02d0d39aa5f9a9ac7f657e8b40fee9369871
Ashish Dulhani <dulhani.ashish@gmail.com>
2017-06-18 00:46:38 +0530

    feat: get stories from stories reducer into Home component

--- src/bundles/Home/components/Home/index.js
+++ src/bundles/Home/components/Home/index.js
@@ -4,21 +4,19 @@ import {
   Text,
   View
 } from 'react-native';
+import StoriesList from './components/StoriesList'; 
 
 export default class Home extends Component {
+  
   render() {
+
+    const {
+      stories
+    } = this.props
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Component Home!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+          <StoriesList stories={stories} />
       </View>
     );
   }
@@ -27,18 +25,6 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
--- src/bundles/Home/containers/HomeContainer/index.js
+++ src/bundles/Home/containers/HomeContainer/index.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Home from '../../components/Home'
-
+import data from './data';
+import { getStories } from '../../../../actions/storiesAction';
 
 class HomeContainer extends Component {
 
   props: Props
 
+  componentWillMount() {
+    // To be called when we got response from server
+    this.props.getStories(data.stories);
+  }
+
   render() {
     return (
-      <Home {...this.props }  />
+      <Home stories={this.props.stories} />
     )
   }
 
 }
 
 
-const mapStateToProps = (state) => {
-  return {
-  }
-}
+const mapStateToProps = (state) => ({
+  stories: state.stories.stories,
+});
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    }
-  }
+const mapDispatchToProps = (dispatch) => ({
+  getStories: stories => dispatch(getStories(stories))
+});
 
-export default connect(null,null)(HomeContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(HomeContainer)
